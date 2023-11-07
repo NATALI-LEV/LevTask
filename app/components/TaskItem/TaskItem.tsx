0e34f099dda11b8b061c793a20dc28c5929d0888
NATALI-LEV <natalillev@gmail.com>
2023-11-07 22:48:27 +0200

    fixed bugs

--- app/components/TaskItem/TaskItem.tsx
+++ app/components/TaskItem/TaskItem.tsx
@@ -50,7 +50,7 @@ function TaskItem({ title, description, date, isCompleted, id }: Props) {
             Incomplete
           </button>
         )}
-        <button className="edit">{edit}</button>
+        
         <button
           className="delete"
           onClick={() => {
@@ -101,7 +101,7 @@ const TaskItemStyled = styled.div`
       }
     }
 
-    .edit {
+    .delete {
       margin-left: auto;
     }
 
