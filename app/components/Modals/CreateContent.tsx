b8235db9ed3702203c2aafbc10929e3d6cc1684c
NATALI-LEV <natalillev@gmail.com>
2023-11-18 20:08:01 +0200

    fixed info

--- app/components/Modals/CreateContent.tsx
+++ app/components/Modals/CreateContent.tsx
@@ -78,7 +78,7 @@ function CreateContent() {
           value={title}
           name="title"
           onChange={handleChange("title")}
-          placeholder="e.g, Watch a video from Fireship."
+          placeholder="such as - study for the exam!"
         />
       </div>
       <div className="input-control">
@@ -89,7 +89,7 @@ function CreateContent() {
           name="description"
           id="description"
           rows={4}
-          placeholder="e.g, Watch a video about Next.js Auth"
+          placeholder="e.g, study for the probability exam , chapters 5-12"
         ></textarea>
       </div>
       <div className="input-control">
